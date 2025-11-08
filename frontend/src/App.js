efae18e1d1a5c9752ec6bba49b5953496f63bd17
Atharva <atharvabhise1@gmail.com>
2025-11-08 21:42:31 +0530

    Initial commit for frontend with Tailwind setup

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900 text-white text-3xl font-bold">
+      Task Management App
+    </div>
+  );
+}
+
+export default App;
