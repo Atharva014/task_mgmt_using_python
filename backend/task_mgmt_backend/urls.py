c93cd753db6a60787f05a63d605608033ac69133
Atharva <atharvabhise1@gmail.com>
2025-11-30 23:09:15 +0530

    Ingress updated.

--- backend/task_mgmt_backend/urls.py
+++ backend/task_mgmt_backend/urls.py
@@ -21,7 +21,7 @@ def health_check(request):
     return JsonResponse({'status': 'healthy'})
 
 urlpatterns = [
-    path('health/', health_check),
+     path('api/health/', health_check),
     path('admin/', admin.site.urls),
     path('api/', include('tasks.urls')),
     path('api/users/', include('users.urls')),
